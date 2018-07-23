44df1074505fb758a08b0c6410dfd0e9f90e90c2
sonyapieklik <piesonya@gmail.com>
2018-07-23 19:02:57 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -2,22 +2,29 @@
 # Code your methods below
 def proper_stance
   puts "Plant legs far apart, bend knees slightly and keep posture loose"
+end
 def base_footwork
   puts "Lift right foot
         Return right foot to the ground
         Finishing with a small skip-step backward"
+      end
 def skip_step_left
   puts "Lower left foot to the ground
 Bounce left foot back up slightly, kicking it a few inches back
 Left, Right, Left and Left
 "
+end
 def skip_step_right 
   puts "Lower right foot to the ground
 Bounce right foot back up slightly, kicking it a few inches back
 Right, Left, Right and Right"
+end
 def lasso 
   "Start arm movement by holding left arm so the back of your wrist is near your chin, pointing your left elbow straight left.
 Lift right arm up and point right elbow diagonally to the right.
 Raise right forearm so that it points straight up, and whip it in small circles to the beat of the song, as if you were a cowboy holding a rope lasso. This movement is repeated eight times as well."
+end
+
+proper_stance
 
 
