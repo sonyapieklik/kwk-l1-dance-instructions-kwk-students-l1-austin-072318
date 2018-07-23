e77fde0db883c08534a4887b827384eb5b40d823
sonyapieklik <piesonya@gmail.com>
2018-07-23 18:57:08 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -3,6 +3,9 @@
 def proper_stance
   puts "Plant legs far apart, bend knees slightly and keep posture loose"
 def base_footwork
-  puts "Lift right foot"
-  
+  puts "Lift right foot
+        Return right foot to the ground
+        Finishing with a small skip-step backward"
+def skip_step_left
+  puts ""
   
