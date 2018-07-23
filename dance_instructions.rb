ffa0eba22f98807776f8d3760d3d6a578179375a
sonyapieklik <piesonya@gmail.com>
2018-07-23 19:20:06 +0000

    Done.

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -1,2 +1,39 @@
 
 # Code your methods below
+def starting_stance
+  puts "Plant legs far apart, bend knees slightly and keep posture loose"
+end
+def base_footwork
+  puts "Lift right foot
+        Return right foot to the ground
+        Finishing with a small skip-step backward"
+      end
+def skip_step(name)
+  puts "Lower"
+  puts "Bounce"
+  puts "Briefly"
+end
+def skip_step_right 
+  puts "Lower right foot to the ground
+Bounce right foot back up slightly, kicking it a few inches back
+Right, Left, Right and Right"
+end
+def lasso 
+  puts "Start"
+puts "Lift"
+puts "Raise" 
+puts "Do"
+end
+
+def bob_the_reins
+  puts "Hold"
+  puts "Cross"
+  puts "Lift"
+  puts "Do"
+end
+
+def bust_a_move
+  puts "Be cool and be snazzy"
+end
+
+
